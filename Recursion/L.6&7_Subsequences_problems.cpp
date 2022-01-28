278f22ab495f2317a114663a2cda90e59dd248e3
CHANDAN1315 <83956516+CHANDAN1315@users.noreply.github.com>
2022-01-28 16:14:55 +0530

    changes and added new  tricks

--- Recursion/L.6&7_Subsequences_problems.cpp
+++ Recursion/L.6&7_Subsequences_problems.cpp
@@ -36,7 +36,7 @@ void subsequence(int i, vector<int> &a, int v[], int n) {
 }
 
 
-/*  2. Print all the subsequnce have the sum as k(given) */
+/*  2. Print all the subsequnce that have the sum equals to k(given) */
 
 void sum_equal_to_k(int ind , int s, vector<int> &v, int a[] , int k, int n ) {
 
@@ -63,3 +63,62 @@ void sum_equal_to_k(int ind , int s, vector<int> &v, int a[] , int k, int n ) {
     sum_equal_to_k(ind + 1, s, v, a, k, n);
 }
 
+// IF you are asked to print only one answer which is equal to k (simillar to que 2)
+bool sum_equal_to_k(int ind , int s, vector<int> &v, int a[] , int k, int n ) {
+
+    if (ind == n) {
+        // condition satisfied
+        if (s == k) {
+            for (auto it : v) cout << it << " ";
+            cout << endl;
+            return true;
+        }
+        // Condition not satisfied
+        return false;
+    }
+
+
+    // taking the element or pick condition
+    v.emplace_back(a[ind]);
+    s += a[ind];
+    // calling the next function
+    /*importan trick used for printing only one answer use of if condintion to use the recursion calls*/
+    if (sum_equal_to_k(ind + 1, s , v, a, k, n) == true) {
+        return true;
+    }
+
+    v.pop_back();
+    s -= a[ind];
+    //not pick condition
+    if (sum_equal_to_k(ind + 1, s, v, a, k, n) == true) {
+        return true;
+    }
+
+    return false ;
+}
+
+
+/*3. Print the count how many subsequence have sum equal to k*/
+// T.C -> 2^n
+int countOf_sum_equal_to_k(int ind , int sum, int a[] , int k, int n ) {
+
+    if(sum > k) return 0; // this case only valid if the elements are (+ve)int.//it will save some recursion calls
+    if (ind == n) {
+        if(sum == k) return 1;
+        else return 0;
+    }
+
+
+    // taking the element
+    sum += a[ind];
+    // calling the next function
+    int l = countOfsum_equal_to_k(ind + 1, sum , a, k, n);
+
+    sum -= a[ind];
+
+    int r = countOfsum_equal_to_k(ind + 1, sum, a, k, n);
+
+    return l+r;
+
+    // future related problem is n- queen
+}
