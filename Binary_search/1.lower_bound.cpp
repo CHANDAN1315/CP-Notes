f2c9664db49a6a76410a57ebe9c69ce5d66ac0a1
CHANDAN1315 <83956516+CHANDAN1315@users.noreply.github.com>
2022-05-17 18:26:58 +0530

    Create 1.lower_bound.cpp

---
+++ Binary_search/1.lower_bound.cpp
@@ -0,0 +1,25 @@
+int lower_bound(int low , int high, vector<int> arr){
+  int low =0, high = n-1;
+  while (low < high) {
+        mid = low + (high - low) / 2;
+ 
+        // If X is less than or equal
+        // to arr[mid], then find in
+        // left subarray
+        if (X <= arr[mid]) {
+            high = mid;
+        }
+ 
+        // If X is greater arr[mid]
+        // then find in right subarray
+        else {
+            low = mid + 1;
+        }
+    }
+   
+    // if X is greater than arr[n-1]
+    if(low < N && arr[low] < X) {
+       low++;
+    }
+    cout<<arr[low]<<endl;
+}
