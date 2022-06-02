d612742f075bd292035aea6b0e2c6d2db3411cd5
CHANDAN1315 <83956516+CHANDAN1315@users.noreply.github.com>
2022-06-02 20:38:08 +0530

    Update L6.maximum_sum_of_not_adjusent_subsequence.cpp

--- Dynamic Programming/L6.maximum_sum_of_not_adjusent_subsequence.cpp
+++ Dynamic Programming/L6.maximum_sum_of_not_adjusent_subsequence.cpp
@@ -3,6 +3,8 @@
   hint : work on pick and not pick condition of subsequence problem of recursion condition.
 
 */  
+
+// Recursive solution.
 int subsequence(int ind , vector<int> &arr) {
     if (ind == 0) return arr[ind];
     if (ind < 0) return 0;
@@ -20,6 +22,31 @@ int main () {
     vector<int> arr(n);
     for (int i = 0; i < n; i++) cin >> arr[i];
     cout << subsequence(n-1, arr);
+  
+  return 0;
+}
+
+
+// Mimozation solution
+int subsequence(int ind , vector<int> &arr, vector<int> &dp) {
+    if (ind == 0) return arr[ind];
+    if (ind < 0) return 0;
+    if(dp[ind] != -1) return dp[ind];
+  
+    int pick = arr[ind] + subsequence(ind - 2, arr, dp);
+    int not_pick = 0 + subsequence(ind - 1, arr, dp);
+
+    return dp[ind] = max(pick, not_pick);
+}
+
+
+int main () {
+
+    int n; cin >> n;
+    vector<int> arr(n);
+    vector<int> dp(n,-1);
+    for (int i = 0; i < n; i++) cin >> arr[i];
+    cout << subsequence(n-1, arr, dp);
   
   return 0;
 }
