537bc2c60fbe72b12153c336dcc416ba23f5c8e3
CHANDAN1315 <83956516+CHANDAN1315@users.noreply.github.com>
2022-06-09 11:56:12 +0530

    Update kadane's algorithm.cpp

--- Techniques/kadane's algorithm.cpp
+++ Techniques/kadane's algorithm.cpp
@@ -1,3 +1,16 @@
+// problem link : https://www.geeksforgeeks.org/maximum-subarray-sum-using-divide-and-conquer-algorithm/
+/*
+problem : maximum subarray sum 
+
+3 solution discuss 
+1. Generating all the subarrays/substrings and finding the max one O(N^3)
+2.nerating all the subarrays/substrings and finding the max one O(N^2
+3.Kadane's Algo.
+
+
+*/
+
+
 void solve() {
 
     int n ; cin >> n;
@@ -5,31 +18,33 @@ void solve() {
     read(a);
     int maxx = 0;
     int leng = 1;
-    // for (int leng = 1; leng <= n; leng++) {
-    //     for (int i = 0; i < n; i++) {
-    //         int sum = 0;
-    //         for (int j = i; j < leng; j++) {
-    //             sum += a[j];
-    //             maxx = max(maxx, sum);
-    //         }
-    //     }
-    // }
+    
+// Method 1.
+    for (int leng = 1; leng <= n; leng++) {
+        for (int i = 0; i < n; i++) {
+            int sum = 0;
+            for (int j = i; j < leng; j++) {
+                sum += a[j];
+                maxx = max(maxx, sum);
+            }
+        }
+    }
 
+// Method 2.
+    for (int i = 0; i < n; i++) {
+        int sum = 0;
+        if (a[i] > 0) {
+            sum += a[i];
+            for (int j = i + 1; j < n; j++) {
+                // in this part we are doing extra itterations for the same included elements with smaller length.
+                if (a[j] < 0) break;
+                else sum += a[j];
+                maxx = max(sum , maxx);
+            }
+        }
+    }
 
-    // for (int i = 0; i < n; i++) {
-    //     int sum = 0;
-    //     if (a[i] > 0) {
-    //         sum += a[i];
-    //         for (int j = i + 1; j < n; j++) {
-    //             // in this part we are doing extra itterations for the same included elements with smaller length.
-    //             if (a[j] < 0) break;
-    //             else sum += a[j];
-    //             maxx = max(sum , maxx);
-    //         }
-    //     }
-    // }
-
-    // This is kadane's algrothium
+// Method 3. kadane's algrothium
 
     int ans = 0;
     int sum = 0;
