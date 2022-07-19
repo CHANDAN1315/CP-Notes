13776563d51b52331d8fde50d72c1417ac9cf6c8
CHANDAN1315 <83956516+CHANDAN1315@users.noreply.github.com>
2022-07-19 12:39:47 +0530

    Create 11. Right view of BT.cpp

---
+++ Trees/Binary_Trees/11. Right view of BT.cpp
@@ -0,0 +1,39 @@
+// Problem link : https://leetcode.com/problems/binary-tree-right-side-view/
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+// n = number of nodes
+// TC : O(n)   SC: O(n)
+class Solution {
+public:
+    
+    
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int> ans;
+        if(root== NULL) return ans;
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty()){
+            int sz = q.size();
+            for(int i =0; i<sz; i++){
+                TreeNode* node = q.front();
+                q.pop();
+                if(i == sz-1) ans.push_back(node->val);
+                
+                if(node->left) q.push(node->left);
+                if(node->right) q.push(node->right);
+            }
+        }
+        return ans;
+        
+    }
+
+};
