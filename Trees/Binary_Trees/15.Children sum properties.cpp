4dcaf2833304536a90e62db01e52366d42d5aaf1
CHANDAN1315 <83956516+CHANDAN1315@users.noreply.github.com>
2022-07-23 12:33:42 +0530

    Create 15.Children sum properties.cpp

---
+++ Trees/Binary_Trees/15.Children sum properties.cpp
@@ -0,0 +1,25 @@
+//problem link : https://takeuforward.org/data-structure/check-for-children-sum-property-in-a-binary-tree/
+
+void reorder(node * root) {
+  if (root == NULL) return;
+  int child = 0;
+  if (root -> left) {
+    child += root -> left -> data;
+  }
+  if (root -> right) {
+    child += root -> right -> data;
+  }
+
+  if (child < root -> data) {
+    if (root -> left) root -> left -> data = root -> data;
+    else if (root -> right) root -> right -> data = root -> data;
+  }
+
+  reorder(root -> left);
+  reorder(root -> right);
+
+  int tot = 0;
+  if (root -> left) tot += root -> left -> data;
+  if (root -> right) tot += root -> right -> data;
+  if (root -> left || root -> right) root -> data = tot;
+}
