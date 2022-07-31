7ab6d631db02e79d8cbd408f9e64d79c953cc002
CHANDAN1315 <83956516+CHANDAN1315@users.noreply.github.com>
2022-07-31 16:51:51 +0530

    Create 21.Morris Traversal.cpp

---
+++ Trees/Binary_Trees/21.Morris Traversal.cpp
@@ -0,0 +1,42 @@
+// problem :https://leetcode.com/problems/binary-tree-inorder-traversal/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> inorder;
+        TreeNode* cur = root;
+        while(cur != NULL){
+            if(cur->left == NULL){
+                inorder.push_back(cur->val);
+                cur= cur->right;
+            }
+            else{
+                TreeNode* pre = cur->left;
+                while(pre->right and pre->right != cur){
+                    pre = pre->right;
+                }
+                if(pre->right == NULL){
+                    pre->right = cur; // creating the thread
+                    cur = cur->left;
+                }
+                else{
+                    pre->right = NULL; // Removing the thread
+                    inorder.push_back(cur->val);
+                    cur = cur->right;
+                }
+            }
+        }
+        return inorder;
+    }
+};
